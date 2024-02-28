6df39bfee5fed464c256341c193034d7cfeb3412
yeasin550 <yeasin51570@gmail.com>
2024-02-28 20:52:08 +0600

    error page setup

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,10 +1,17 @@
-
+"use client"
+import Link from "next/link";
 
 const NotFound = () => {
     return (
         <div className="w-40 mx-auto mt-12 text-center">
             <h1 className="text-5xl">404</h1>
             <p className="text-3xl">Not Found</p>
+             <Link
+              href="/"
+              className="bg-purple-700 py-2 px-4 rounded-md"
+            >
+              Back To Home
+            </Link>
         </div>
     );
 };
